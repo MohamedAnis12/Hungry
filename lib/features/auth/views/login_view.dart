8c20ad40c23149a97bed3afaf2c30daf6c50fbb0
anis <vidotyobegaming@gmail.com>
2026-06-24 15:24:15 +0300

    Enhance login view with custom text form and logo integration

--- lib/features/auth/views/login_view.dart
+++ lib/features/auth/views/login_view.dart
@@ -1,15 +1,51 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
+import 'package:hungry/core/constants/app_colors.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [
-
-
+    return Scaffold(
+      backgroundColor: AppColors.appColor,
       
-    ]));
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Center(
+          child: Column(children: [
+            Gap(150),
+            SvgPicture.asset("assets/logo.svg")
+          ,Gap(30),
+
+          CustomTextForm()
+              
+          
+              ]),
+        ),
+      ));
+  }
+}
+
+
+class CustomTextForm extends StatelessWidget {
+  const CustomTextForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.white,
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white
+            
+          )
+        )
+      ),
+    );
   }
 }
