ba18f23c4c3bb05504cb87b54fde49957394096d
anis <vidotyobegaming@gmail.com>
2026-06-25 03:51:20 +0300

    Refactor login view to use stateful widget and implement custom text form fields for email and password

--- lib/features/auth/views/login_view.dart
+++ lib/features/auth/views/login_view.dart
@@ -1,50 +1,57 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
 import 'package:hungry/core/constants/app_colors.dart';
+import 'package:hungry/features/auth/widgets/custom_text_form.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
   const LoginView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.appColor,
-      
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Center(
-          child: Column(children: [
-            Gap(150),
-            SvgPicture.asset("assets/logo.svg")
-          ,Gap(30),
-
-          CustomTextForm()
-              
-          
-              ]),
-        ),
-      ));
-  }
+  State<LoginView> createState() => _LoginViewState();
 }
 
-
-class CustomTextForm extends StatelessWidget {
-  const CustomTextForm({super.key});
+class _LoginViewState extends State<LoginView> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      decoration: InputDecoration(
-        filled: true,
-        fillColor: Colors.white,
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.white
-            
-          )
-        )
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        backgroundColor: AppColors.appColor,
+
+        body: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Center(
+              child: Column(
+                children: [
+                  Gap(150),
+                  SvgPicture.asset("assets/logo.svg"),
+                  Gap(30),
+
+                  CustomTextForm(
+                    isPassword: false,
+                    controller: _emailController,
+                    text: "Email address",
+                  ),
+                  Gap(10),
+                  CustomTextForm(
+                    isPassword: true,
+                    controller: _passwordController,
+                    text: "Password ***",
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
---
+++ lib/features/auth/widgets/custom_text_form.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hungry/core/constants/app_colors.dart';
+
+class CustomTextForm extends StatefulWidget {
+  const CustomTextForm({super.key, required this.isPassword, required this._controller, required this.text});
+  final bool isPassword;
+  final TextEditingController _controller;
+  final String text;
+  @override
+  State<CustomTextForm> createState() => _CustomTextFormState();
+}
+
+class _CustomTextFormState extends State<CustomTextForm> {
+  late bool openPassword = widget.isPassword;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget._controller,
+      obscureText: openPassword,
+
+      ///style
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.white,
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red),
+        ),
+
+        hintText: widget.text,
+        suffixIcon: widget.isPassword
+            ? GestureDetector(
+                onTap: () {
+                  setState(() {
+                    openPassword = !openPassword;
+                  });
+                },
+                child: Icon(
+                  openPassword == true
+                      ? Icons.remove_red_eye_rounded
+                      : Icons.visibility_off_outlined,
+                  color: AppColors.appColor,
+                ),
+              )
+            : Icon(Icons.arrow_back, color: Colors.white),
+        //errorText: "some thing went wrong",
+      ),
+    );
+  }
+}
