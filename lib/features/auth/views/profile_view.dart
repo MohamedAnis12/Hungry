691157721c01a184e67efcd36eefee4d03ae3817
anis <vidotyobegaming@gmail.com>
2026-07-23 23:46:24 +0300

    Add layout and structure to ProfileView, including a circular container and CustomPersonalInfo widget

--- lib/features/auth/views/profile_view.dart
+++ lib/features/auth/views/profile_view.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:hungry/core/constants/app_strings.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Gap(AppStrings.startGap),
+            Container(
+              height: 150,
+              width: 150,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(2000),
+              ),
+              
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomPersonalInfo extends StatelessWidget {
+  const CustomPersonalInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
